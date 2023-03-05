63c5844a603505846028513b418cb66013e33ed7
Sidiqherman <asidiq740@gmail.com>
2023-03-05 16:21:55 +0700

    TUGAS 01

---
+++ Tugas Prokom1.py
@@ -0,0 +1,17 @@
+x = str(input("Nama : "))
+y = int(input("Golongan : "))
+z = int(input("Kelompok Jam Kerja : "))
+
+print()
+print("=============================")
+print()
+gaji_pokok = y * 1000000
+gaji_lembur =z * 200000
+pendapatan = gaji_pokok + gaji_lembur
+
+print("Nama : ", x)
+print("Golongan : ", y)
+print("Gaji pokok : ", gaji_pokok)
+print("Gaji lembur : ", gaji_lembur)
+print("pendapatan ", pendapatan)
+print()
