45757631adb147bb777095a6953a59d30836bb34
Sidiqherman <asidiq740@gmail.com>
2023-03-09 13:15:52 +0700

    Exercise-01

--- Tugas 01.py
+++ Tugas 01.py
@@ -1,3 +1,6 @@
 print("Achmad Sidiq Herman")
-print("I0322001")
-print("Saya berasal dari Kota Bontang, sebuah kota di Provinsi Kalimantan Timur. Di Kota Bontang, kami tidak mengenal musim kemarau dan penghujan, mungkin ini dikarenakan letaknya yang berdekatan dengan garis khatulistiwa. Selain itu, Kota Bontang juga berbatasan langsung dengan wilayah laut sehingga memiliki pantai dan memiliki sumber daya laut seperti ikan, udang, dan makanan laut lainnya")
+print("I0322001\n")
+
+print("Saya berasal dari Kota Bontang, sebuah kota di Provinsi Kalimantan Timur. Di Kota Bontang, kami tidak mengenal musim kemarau dan penghujan, mungkin ini dikarenakan letaknya yang berdekatan dengan garis khatulistiwa. Selain itu, Kota Bontang juga berbatasan langsung dengan wilayah laut sehingga memiliki pantai dan memiliki sumber daya laut seperti ikan, udang, dan makanan laut lainnya\n")
+
+print("Saat ini saya sedang menjalani perkuliahan sebagai mahasiswa program studi Teknik Industri di Universitas Sebelas Maret. Setelah lulus dari sini, saya mempunyai keinginan untuk dapat mengamalkan ilmu ilmu yang saya dapatkan semasa berkuliah, saya juga ingin mendapatkan pekerjaan yang layak guna menghidupi diri sendiri dan keluarga nantinya")
