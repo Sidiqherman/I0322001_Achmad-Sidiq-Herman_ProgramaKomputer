cf7a0cd9316a47664c3d2d3dc2c9b5dd24e4bd41
Sidiqherman <asidiq740@gmail.com>
2023-03-09 13:02:49 +0700

    huhu

--- anjay.py
+++ anjay.py
@@ -1,3 +1,3 @@
 print("hello world")
 print ("hahahahaha")
-print("hihihhahahaiihiiiiiiiiiiiiiii hahahaha HHAHAHAHOHIHIHIHIhahahahauuuuuddd")
+print("hihihhahahaiihiiiiiiiiiiiiiii hahahaha hhuhuhu HHAHAHAHOHIHIHIHIhahahahauuuuuddd")
