7fdba79e20f1b994344e16937f3478715e3e3068
Sidiqherman <asidiq740@gmail.com>
2023-03-11 09:22:50 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,8 @@
+print("""
+Nama : Achmad Sidiq Herman
+Nim : I0322001
+
+    Saya berasal dari Kota Bontang, sebuah kota di Provinsi Kalimantan Timur. Di Kota Bontang, kami tidak mengenal musim kemarau dan penghujan, mungkin ini dikarenakan letaknya yang berdekatan dengan garis khatulistiwa. Selain itu, Kota Bontang juga berbatasan langsung dengan wilayah laut sehingga memiliki pantai dan memiliki sumber daya laut seperti ikan, udang, dan makanan laut lainnya. Kota Bontang memliki suhu yang cukup panas tak seperti di Kota Surakarta yang menurut saya lebih dingin.
+
+    Saat ini saya sedang menjalani perkuliahan sebagai mahasiswa program studi Teknik Industri di Universitas Sebelas Maret. Setelah lulus dari sini, saya mempunyai keinginan untuk dapat mengamalkan ilmu ilmu yang saya dapatkan semasa berkuliah, saya juga ingin mendapatkan pekerjaan yang layak guna menghidupi diri sendiri dan keluarga nantinya. Saya bercita-cita untuk dapat bekerja di bidang pertambangan di salah satu perusahaan tambang di kota saya.
+""")
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # I0322001 Achmad Sidiq Herman
-Sidiq Punya
